db6326307e0c902d7cbe90b507b980e000c2f79b
molumolua <1005389104@qq.com>
2026-02-15 19:23:02 +0800

    add qwen3

---
+++ scripts/models/qwen3-4B.sh
@@ -0,0 +1,16 @@
+MODEL_ARGS=(
+   --swiglu
+   --num-layers 36
+   --hidden-size 2560
+   --ffn-hidden-size 9728
+   --num-attention-heads 32
+   --use-rotary-position-embeddings
+   --disable-bias-linear
+   --add-qkv-bias
+   --normalization "RMSNorm"
+   --norm-epsilon 1e-6
+   --rotary-base 1000000
+   --group-query-attention
+   --num-query-groups 8
+   --vocab-size 151936
+)
